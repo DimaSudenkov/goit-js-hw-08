41063c3bb5246be65277ed6f775ba946f8aab88c
DimaSudenkov <sudenkovdimas@gmail.com>
2023-10-09 20:22:05 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const keyEmailMessage = "feedback-form-state";
+
+form.addEventListener('input', throttle(handlerInput, 500));
+
+let dataForm = JSON.parse(localStorage.getItem(keyEmailMessage)) || {};
+const { email, message } = form.elements;
+
+function handlerInput() {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(keyEmailMessage, JSON.stringify(dataForm));
+}
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault()
+  const { email, message } = evt.currentTarget.elements;
+  console.log({ email: email.value, message: message.value });
+  localStorage.removeItem(keyEmailMessage);
+  evt.currentTarget.reset();
+}
